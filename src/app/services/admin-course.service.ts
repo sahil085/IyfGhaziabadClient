15abc6d179347391ea5a861a4c2de609b46f90dc
Ravi <ravi97garg@gmail.com>
2018-06-28 00:46:44 +0530

    HomePage and AdminPanel created

--- src/app/services/admin-course.service.ts
+++ src/app/services/admin-course.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { environment } from '../../environments/environment';
 
-// @Injectable({
+@Injectable()
 //   providedIn: 'root'
 // })
 export class AdminCourseService {
