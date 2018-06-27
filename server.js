4c034fbb66916438e7d0f4b4b9c75be27678ae93
sahil085 <vermasahil269@gmail.com>
2018-06-27 22:18:35 +0530

    removing --aot from post install

--- server.js
+++ server.js
@@ -13,24 +13,24 @@ app.get('/*', function(req,res) {
 });
 
 // Start the app by listening on the default Heroku port
-if (process.env.NODE_ENV == 'production') {
-  app.use(function (req, res, next) {
-    res.setHeader('Strict-Transport-Security', 'max-age=8640000; includeSubDomains');
-    if (req.headers['x-forwarded-proto'] && req.headers['x-forwarded-proto'] === "http") {
-      return res.redirect(301, 'https://' + req.host + req.url);
-    } else {
-      return next();
-    }
-  });
-} else {
-  app.use(function (req, res, next) {
-    res.setHeader('Strict-Transport-Security', 'max-age=8640000; includeSubDomains');
-    if (!req.secure) {
-      return res.redirect(301, 'https://' + req.host + ":" + process.env.PORT + req.url);
-    } else {
-      return next();
-    }
-  });
-}
+// if (process.env.NODE_ENV == 'production') {
+//   app.use(function (req, res, next) {
+//     res.setHeader('Strict-Transport-Security', 'max-age=8640000; includeSubDomains');
+//     if (req.headers['x-forwarded-proto'] && req.headers['x-forwarded-proto'] === "http") {
+//       return res.redirect(301, 'https://' + req.host + req.url);
+//     } else {
+//       return next();
+//     }
+//   });
+// } else {
+//   app.use(function (req, res, next) {
+//     res.setHeader('Strict-Transport-Security', 'max-age=8640000; includeSubDomains');
+//     if (!req.secure) {
+//       return res.redirect(301, 'https://' + req.host + ":" + process.env.PORT + req.url);
+//     } else {
+//       return next();
+//     }
+//   });
+// }
 
 app.listen(process.env.PORT || 8090);
