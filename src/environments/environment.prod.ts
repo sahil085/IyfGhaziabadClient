ae5417d155f52855310373dfae40cb285faae9ae
sahil085 <vermasahil269@gmail.com>
2018-07-31 17:10:40 +0530

    to be deployed on prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://ec2-18-217-142-137.us-east-2.compute.amazonaws.com'
+  apiUrl: 'http://ec2-18-222-231-197.us-east-2.compute.amazonaws.com:8080'
 };
